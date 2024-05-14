ce3f93ac361bd33bb084669038c4f715979e33a5
IAntigrippinI <kovalskii.bi@dvfu.ru>
2024-05-14 18:42:15 +1000

    add some global names

--- src/config.py
+++ src/config.py
@@ -1,5 +1,12 @@
-BASE_URL = "https://vladivostok.hh.ru/search/vacancy?text=&area=22&hhtmFrom=main&hhtmFromLabel=vacancy_search_line"
-
-URL_FOR_GET_SPECIALIZATION = "https://vladivostok.hh.ru/search/vacancy/advanced?no_default_area=&area=22&ored_clusters=true&hhtmFrom=vacancy_search_list"
+START_URL = "https://vladivostok.hh.ru/search/vacancy?L_save_area=true&text=&excluded_text=&professional_role="
+END_URL = "&area=22&salary=&currency_code=RUR&experience=doesNotMatter&order_by=relevance&search_period=0&items_on_page=50&hhtmFrom=vacancy_search_filter"
 
 SPECIALIZATIONS_FILE_PATH = "src/data/spec.txt"
+
+INFO_TEXT = """Начните вводить название интересующей вас профессии, 
+            нажмите Enter и перед вами появится список похожих профессий, 
+            введите id профессии и ожидайте выполнение запроса
+            Либо же вы можете ввести id професси сразу
+            Введите 'exit' для выхода"""
+
+LIMIT_SPECIALIZATION_OUTPUT = 7
