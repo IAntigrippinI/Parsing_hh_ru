27a220a7a19d72547572e50e741483c26f22fd70
IAntigrippinI <kovalskii.bi@dvfu.ru>
2024-05-20 23:49:01 +1000

    refactor code

--- src/main.py
+++ src/main.py
@@ -1,29 +1,23 @@
-from collecting_data import collect_data
-from find_sim_fum import find_similar
+import json
+
+from req import get_vacancies
+from FindSimilar import find_similar
+
+from init import avilable_roles
 from config import INFO_TEXT
 
 
-def start():
-    print("in start")
-    print(INFO_TEXT)
-    for i in range(1, 3):
-        specialization_for_find = input("Ввод: ")
-        if specialization_for_find == "exit":
-            break
-        elif specialization_for_find.isdigit():
-            collect_data(int(specialization_for_find))
-        else:
-            print(find_similar(specialization_for_find.lower()))
-
-
 def main():
-    start()
+    print(INFO_TEXT)
+    while True:
+        inp = input("Ввод: ")
+        if inp == "q":
+            break
+        elif inp.replace(" ", "").isdigit():
+            print(get_vacancies(int(inp.replace(" ", ""))))
+        else:
+            print(find_similar(inp.lower()))
 
 
 if __name__ == "__main__":
-
-    import os
-
-    if not os.path.exists("data"):
-        os.makedirs("data")
     main()
