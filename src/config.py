4a37a85795d4857cc98b9757393cf36ab2a5aabb
IAntigrippinI <kovalskii.bi@dvfu.ru>
2024-05-20 23:47:13 +1000

    add urls

--- src/config.py
+++ src/config.py
@@ -1,5 +1,10 @@
+URL_FOR_GET_PROFESSIONAL_ROLES = "https://api.hh.ru/professional_roles"
+
 START_URL = "https://vladivostok.hh.ru/search/vacancy?L_save_area=true&text=&excluded_text=&professional_role="
-END_URL = "&area=22&salary=&currency_code=RUR&experience=doesNotMatter&order_by=relevance&search_period=0&items_on_page=50&hhtmFrom=vacancy_search_filter"
+END_URL = "&area=22&salary=&currency_code=RUR&experience=doesNotMatter&order_by=relevance&search_period=0&items_on_page=20&hhtmFrom=vacancy_search_filter"
+
+START_URL_FOR_PAGES = "https://vladivostok.hh.ru/search/vacancy?L_save_area=true&area=22&items_on_page=20&search_field=name&search_field=company_name&search_field=description&enable_snippets=false&professional_role="
+END_URL_FOR_PAGES = "&text=&page="
 
 SPECIALIZATIONS_FILE_PATH = "src/data/spec.txt"
 
@@ -7,6 +12,6 @@ INFO_TEXT = """Начните вводить название интересую
             нажмите Enter и перед вами появится список похожих профессий, 
             введите id профессии и ожидайте выполнение запроса
             Либо же вы можете ввести id професси сразу
-            Введите 'exit' для выхода"""
+            Введите 'q' для выхода"""
 
 LIMIT_SPECIALIZATION_OUTPUT = 7
